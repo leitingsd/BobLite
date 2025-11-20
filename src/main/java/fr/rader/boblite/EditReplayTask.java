45e5be62fdafd31071880b69d6d6830228791d62
Hoshino Satoru <47239222+leitingsd@users.noreply.github.com>
2025-11-21 01:44:13 +0800

    update to 1.20.4

--- src/main/java/fr/rader/boblite/EditReplayTask.java
+++ src/main/java/fr/rader/boblite/EditReplayTask.java
@@ -169,10 +169,25 @@ public class EditReplayTask implements Runnable {
                 chatPacketID = 0x31;
                 break;
             case "1.19.4":
+            case "1.20":
+            case "1.20.1":
                 timePacketID = 0x5E;
                 weatherPacketID = 0x1F;
                 spawnPositionID = 0x50;
                 chatPacketID = 0x35;
+                break;
+            case "1.20.2":
+                timePacketID = 0x60;
+                weatherPacketID = 0x20;
+                spawnPositionID = 0x52;
+                chatPacketID = 0x37;
+                break;
+            case "1.20.3":
+            case "1.20.4":
+                timePacketID = 0x62;
+                weatherPacketID = 0x20;
+                spawnPositionID = 0x54;
+                chatPacketID = 0x37;
                 break;
 
             // we show an error and stop if the protocol isn't supported
